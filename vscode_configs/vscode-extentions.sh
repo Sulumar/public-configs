2a6ec9facf7a90797b08de1f40afd99efa747bd9
Sulumar <the01geek@gmail.com>
2021-05-06 22:47:21 +0200

    my vscode config and workspace for CS50s Final Project 2021

---
+++ vscode_configs/vscode-extentions.sh
@@ -0,0 +1,13 @@
+code --install-extension ecmel.vscode-html-css
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.code-runner
+code --install-extension KevinRose.vsc-python-indent
+code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
+code --install-extension magicstack.MagicPython
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension samuelcolvin.jinjahtml
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension wholroyd.jinja
